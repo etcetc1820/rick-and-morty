3e51ed8272029ca9aac85ba34f45a3d91a41abd9
Igor Naydenov <etcetc1820@gmail.com>
2021-06-18 12:53:58 +0300

    added saga

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,8 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { charactersReducer } from "../common/Characters/characters.slice";
+import createSagaMiddleware from "redux-saga";
+
+const middleware = getDefaultMiddleware();
+const sagaMiddleware = createSagaMiddleware();
+
+middleware.push(sagaMiddleware);
 
 export const store = configureStore({
   reducer: {
     charactersReducer,
   },
+  middleware,
 });
